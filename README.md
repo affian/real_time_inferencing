d2efde0447e7201689cb9a4d4532421548f0206a
affian <nnamdiaffia@gmail.com>
2019-07-01 15:52:15 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# real_time_inferencing
-Live object detection (generic)
+# Real Time Inference
+
+Set up your environment using `environment.yml`. For Conda run: 
+
+`conda env create -f environment.yml`
+
+The name of the environment will be the first line in the .yml file. 
+
+If that fails you, simply `conda install` the following packages:
+- opencv
+- numpy
+- requests
+
+Activate your environment, and start the server with:
+
+`python server.py`
---
+++ drawinghelper.py
@@ -0,0 +1,143 @@
+# import the necessary packages
+import os
+import time
+from threading import Thread
+import requests
+import json
+from collections import deque
+
+import numpy as np
+import logging
+import cv2
+import queue
+
+logger = logging.getLogger("DrawingHelper")
+
+class DrawingHelper:
+    """ Manages the drawing on screen using the inferenced frames. """
+    def __init__(self, VID_HEIGHT, VID_WIDTH):
+        
+        # Thread management and queueing
+        self.stopped = False
+        self.thread = Thread(target=self.update, args=())
+        self.thread.daemon = True
+        self.queue = queue.Queue()
+
+        # Video dimensions
+        self.VID_HEIGHT = VID_HEIGHT
+        self.VID_WIDTH = VID_WIDTH
+
+        # Drawing constants
+        # Possibly replace with a namespace?
+        self.FONT = cv2.FONT_HERSHEY_SIMPLEX
+        self.FONT_SCALE = 0.5        
+        self.THICKNESS = 1
+        self.BB_THICKNESS = 1
+        self.FRAME_THRESHOLD = 60  # This should be an even number.
+
+        # The different font and bbox colours- dependent on cell type.
+        self.FONT_COLOUR_BLUE = (255, 98, 0)
+        self.FONT_COLOUR_RED = (74, 39, 186)
+        self.FONT_COLOUR_YELLOW = (51, 184, 253)
+
+        self.BB_COLOUR_BLUE = (255, 98, 0)   # IBM blue 
+        self.BB_COLOUR_RED = (74, 39, 186)
+        self.BB_COLOUR_YELLOW = (51, 184, 253)       
+
+        #nuclei labels
+        self.cell_type_a = 'epithelial'
+        self.cell_type_b = 'fibroblast'
+        self.cell_type_c = 'lymphocyte'
+
+
+
+    def start(self):
+        # start the thread to read frames from the queue
+        self.thread.start()
+        return self
+
+    def enqueue(self, item):
+        self.queue.put_nowait(item)
+
+    def update(self):
+        try:
+            # keep looping infinitely until the thread is stopped
+            while True:
+                # if the thread indicator variable is set, stop the thread
+                if self.stopped:
+                    return
+
+                try:
+                    inferred_frame = self.queue.get(block=True)
+                    json_resp = inferred_frame['json_resp']
+                    frame_data = inferred_frame['frame']
+
+                    if json_resp:
+                        current_letter = json_resp[0]['label']
+                    else:
+                        current_letter = ''
+                        json_resp = [{'xmin': 0, 'ymin': 0, 'xmax': 0, 'ymax': 0, 'confidence': 0, 'label': ''}]
+
+                    self.draw_bounding_box(json_resp, frame_data)
+
+                    return frame_data
+
+                except queue.Empty:
+                    logger.debug("Slept and nothing to do... Trying again.")
+                    time.sleep(1)
+                    continue
+
+        except Exception as e:
+            logger.error("Exception occurred = {}".format(e))
+
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
+
+    def draw_bounding_box(self, json_resp, image):
+                 
+        for i in range(len(json_resp)):
+            x_max = json_resp[i]['xmax']
+            y_max = json_resp[i]['ymax']
+
+            x_min = json_resp[i]['xmin']
+            y_min = json_resp[i]['ymin']
+
+            #preferred coordinates to place labels directly above bounding boxes
+            y = y_min - 5 if y_min - 5 > 5 else y_min + 5
+
+            #assign bounding box colours to each class
+            if json_resp[i]['label'] == 'epithelial':
+                display_BB_colour = self.BB_COLOUR_BLUE
+
+            elif json_resp[i]['label'] == 'fibroblast':
+                display_BB_colour = self.BB_COLOUR_RED
+
+            else:
+                display_BB_colour = self.BB_COLOUR_YELLOW
+
+            cv2.rectangle(image, (x_max,y_max), (x_min, y_min), display_BB_colour, self.BB_THICKNESS)
+
+            #put text label on bounding box
+            if json_resp[i]['label'] == 'epithelial':
+                display_label = self.cell_type_a
+
+            elif json_resp[i]['label'] == 'fibroblast':
+                display_label = self.cell_type_b
+
+            else: 
+                display_label = self.cell_type_c
+
+            #assign corresponding font colours to each class
+            if json_resp[i]['label'] == 'epithelial':
+                font_colour = self.FONT_COLOUR_BLUE
+
+            elif json_resp[i]['label'] == 'fibroblast':
+                font_colour = self.FONT_COLOUR_RED
+
+            else:
+                font_colour = self.FONT_COLOUR_YELLOW
+
+
+            cv2.putText(image, display_label, (x_min, y), self.FONT, 0.37, font_colour, lineType=cv2.LINE_AA)
+    
